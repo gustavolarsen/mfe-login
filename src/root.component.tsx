aaed3effab934231e2d9baae2e9be3ae33075080
Gustavo Larsen <guslarsen@gmail.com>
2024-10-09 14:04:45 -0300

    first commit

---
+++ src/root.component.tsx
@@ -0,0 +1,11 @@
+export default function Root(props) {
+  return (
+    <>
+      <h1>TELA DE LOGIN</h1>
+      <h1>TELA DE LOGIN</h1>
+      <h1>TELA DE LOGIN</h1>
+      <h1>TELA DE LOGIN</h1>
+      <h1>TELA DE LOGIN</h1>
+    </>
+  );
+}
